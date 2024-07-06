b0e454a75efce10b3d77077d13efc6cd3523ac30
NutSheLL <holnaz1337@gmail.com>
2024-07-06 20:59:06 +0300

    Draft_v2.0

--- Task_3.py
+++ Task_3.py
@@ -4,12 +4,21 @@ import re
 def normalize_phone(phone_number: str):
     # Нормалізує телефонні номери до стандартного формату. 
     # Функція приймає один аргумент - рядок з телефонним номером у будь-якому форматі.
-    replacement = "380"
-    start_pattern = r"[+]"
-    formatted_name = re.sub(start_pattern, replacement, phone_number)
+    first_pattern = r"[+]"
+    second_pattern = r""
+
+    if first_pattern:
+        pattern_all = r"([+])\d"
+        replacement_all = ""
+    elif second_pattern:
+        replacement = "380"
+        formatted_name = re.sub(first_pattern, replacement, phone_number)
+    elif third_pattern:
+        code_replacement = '+38'
+        formatted_code = re.sub(second_pattern, code_replacement, phone_number)
     # match = re.search(pattern, phone_number)
     return print(formatted_name)
 
 
-normalize_phone()
+normalize_phone("+999124567")
     
