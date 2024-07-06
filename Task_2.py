69ef1be92354915952b35693cbf6a8b3b8e444c3
NutSheLL <holnaz1337@gmail.com>
2024-07-06 15:55:19 +0300

    Draft_v3.0

--- Task_2.py
+++ Task_2.py
@@ -4,12 +4,12 @@ import random
 def get_numbers_ticket(min, max, quantity):
     # Генерує набір унікальних випадкових чисел у межах заданих параметрів для лотереї
     try:
-        unique_numbers = random.randint(min, max)
-        ticket_numbers = random.choices(unique_numbers, k = quantity)
-        return print(ticket_numbers)
+        random_numb = random.randint(min, max)
+        unique_numbers = random.sample(range(random_numb), quantity)
+        return print(unique_numbers)
     except Exception as e:
         print(f"An error occured: {e}")
     
 
-lottery_numbers = get_numbers_ticket(1, 49, 5)
+get_numbers_ticket(1, 49, 6)
 # print("Ваші лотерейні числа:", lottery_numbers)
