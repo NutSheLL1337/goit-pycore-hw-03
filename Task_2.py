83e2056e0c03d9d4b169fafbe9f5bcb7e985689b
NutSheLL <holnaz1337@gmail.com>
2024-07-06 14:58:56 +0300

    Draft

---
+++ Task_2.py
@@ -0,0 +1,13 @@
+import random
+
+
+def get_numbers_ticket(min, max, quantity):
+    # Генерує набір унікальних випадкових чисел у межах заданих параметрів для лотереї
+    unique_numb = {}
+    
+    return sorted
+
+
+# get_numbers_ticket(2, 999, 6)
+# lottery_numbers = get_numbers_ticket(1, 49, 6)
+# print("Ваші лотерейні числа:", lottery_numbers)
