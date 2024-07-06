32a45150880bde248b4562bc91ba552d411aa380
NutSheLL <holnaz1337@gmail.com>
2024-07-06 20:17:46 +0300

    Fixed bugs with if(added or)

--- Task_2.py
+++ Task_2.py
@@ -4,15 +4,15 @@ import random
 def get_numbers_ticket(min, max, quantity):
     # Генерує набір унікальних випадкових чисел у межах заданих параметрів для лотереї
     try:
-        random_numb = random.randint(min, max)
-        unique_numbers = random.sample(range(random_numb), quantity)
-        if min < 1 and max > 1000:
-            print("You've entered number out of range, min is 1, and maximum is 1000")
+        if (min < 1 or max > 1000):
+            print("You've entered number out of range, minimum value is 1, and maximum is 1000")
         else:
+            random_numb = random.randint(min, max)
+            unique_numbers = random.sample(range(random_numb), quantity)
             return print(unique_numbers)
     except Exception as e:
         print(f"An error occured: {e}")
     
 
-get_numbers_ticket(1, 49, 6)
+get_numbers_ticket(1, 1001, 5)
 # print("Ваші лотерейні числа:", lottery_numbers)
