3aa2abfa07fda7248ce66bcacf8e67f5592f42c0
NutSheLL <holnaz1337@gmail.com>
2024-07-07 10:28:00 +0300

    Added comments

--- Task_3.py
+++ Task_3.py
@@ -5,12 +5,15 @@ def normalize_phone(phone_number: str):
     # Нормалізує телефонні номери до стандартного формату. 
     # Функція приймає один аргумент - рядок з телефонним номером у будь-якому форматі.
     try:
+         # Видаляємо всі символи, крім цифр та '+'
         normalized_number = re.sub(r'[^\d+]', '', phone_number)
+         # Якщо номер не починається з '+', додаємо '+38'
         if not re.match(r'^\+', normalized_number):
             if re.match(r'^380', normalized_number):
                 normalized_number = '+' + normalized_number
             else:
                 normalized_number = '+38' + normalized_number
+         # Якщо номер починається з '+', але не має '38' після '+'        
         elif not re.match(r'^\+38', normalized_number):
             normalized_number = '+38' + normalized_number[1:]  
         return normalized_number        
