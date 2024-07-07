6fd4180850ed23ac6dabc3edd9bcf80e715cadaf
NutSheLL <holnaz1337@gmail.com>
2024-07-07 09:46:38 +0300

    Added match groups

--- Task_3.py
+++ Task_3.py
@@ -5,29 +5,19 @@ def normalize_phone(phone_number: str):
     # Нормалізує телефонні номери до стандартного формату. 
     # Функція приймає один аргумент - рядок з телефонним номером у будь-якому форматі.
     try:
-        if first_pattern:
-            first_pattern = r"([+])\d"
-            replacement_first = ""
-            formatted_phone = re.sub(first_pattern, replacement_first, phone_number)
-        elif second_pattern:
-            second_pattern = r"[+]"
-            replacement = "380"
-            formatted_plus = re.sub(first_pattern, replacement, phone_number)
-        elif third_pattern:
-            third_pattern = r"\w[38]"
-            code_replacement = '+38'
-            formatted_code = re.sub(second_pattern, code_replacement, phone_number)
+        first_pattern = r"([\++])(\d+[38])"
+        match = re.search(first_pattern, phone_number)
+        if match:
+            print("Знайдено:", match.group(1), match.group(2))
+        else:
+            print("Не знайдено plus або +38.")
+            phone_number += "+38"
+        return phone_number    
+        sub_pattern = r"\s"
+        replacement = "_"
+        match_digits = re.sub(sub_pattern, replacement, phone_number)    
     except Exception as e:
         print(f"An error occured: {e}")
-        
 
-
-
-    # match = re.search(pattern, phone_number)
-    return print(formatted_name)
-
-    
-
-
-normalize_phone("+999124567")
+print(normalize_phone("0999124567"))
     
