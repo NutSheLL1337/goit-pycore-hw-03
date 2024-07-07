ae8bdf4a23f54f75e1286f5379a03c5e7db12847
NutSheLL <holnaz1337@gmail.com>
2024-07-07 10:34:38 +0300

    Changed method to search

--- Task_3.py
+++ Task_3.py
@@ -1,24 +1,25 @@
 import re
 
 
-def normalize_phone(phone_number: str):
+def normalize_phone(phone_number: str) -> str:
     # Нормалізує телефонні номери до стандартного формату. 
     # Функція приймає один аргумент - рядок з телефонним номером у будь-якому форматі.
     try:
-         # Видаляємо всі символи, крім цифр та '+'
+        # Видаляємо всі символи, крім цифр та '+'
         normalized_number = re.sub(r'[^\d+]', '', phone_number)
-         # Якщо номер не починається з '+', додаємо '+38'
-        if not re.match(r'^\+', normalized_number):
-            if re.match(r'^380', normalized_number):
+    
+        # Якщо номер не починається з '+', додаємо '+38'
+        if not re.search(r'^\+', normalized_number):
+            if re.search(r'^380', normalized_number):
                 normalized_number = '+' + normalized_number
             else:
                 normalized_number = '+38' + normalized_number
-         # Якщо номер починається з '+', але не має '38' після '+'        
-        elif not re.match(r'^\+38', normalized_number):
-            normalized_number = '+38' + normalized_number[1:]  
-        return normalized_number        
+        # Якщо номер починається з '+', але не містить '38' після '+'
+        elif not re.search(r'^\+38', normalized_number):
+            normalized_number = '+38' + normalized_number[1:]
+        return normalized_number   
     except Exception as e:
         print(f"An error occured: {e}")
 
-print(normalize_phone("999124567"))
+print(normalize_phone("asd999124567"))
     
